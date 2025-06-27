cf6a0f9c27f38aedb4e42d62d315d4c19c076271
Vishal thakur <vishal.matchbest@gmail.com>
2025-06-27 11:12:34 +0530

    get route updated

--- index.js
+++ index.js
@@ -29,9 +29,17 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log('Connected to MongoDB Local'))
 .catch(err => console.error('MongoDB Connection Error:', err));
 
+
+app.get('/', (req, res) => {
+    res.json({
+        success: true,
+        message: "Your server is up and running...."
+    });
+})
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
 
 module.exports = app
